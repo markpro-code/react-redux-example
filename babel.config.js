32fc29a14dfca91651132629c02dd28b996618c1
Mark J <markpro.code@gmail.com>
2020-09-16 18:57:40 +0800

    update for lint

--- babel.config.js
+++ babel.config.js
@@ -1,44 +1,56 @@
+/* eslint-env node  */
+const mode = process.env.NODE_ENV;
+const isProd = mode === 'production';
+
 module.exports = function (api) {
-    api.cache(true)
+  api.cache(true);
+  return {
+    presets: [
+      '@babel/preset-react',
+      [
+        '@babel/preset-env',
+        {
+          forceAllTransforms: true,
+          useBuiltIns: 'entry',
+          corejs: '2',
+        },
+      ],
+    ],
 
-    return {
-        presets: [
-            '@babel/preset-react',
-            [
-                '@babel/preset-env',
-                {
-                    forceAllTransforms: true,
-                    useBuiltIns: 'entry',
-                    corejs: '2',
-                },
-            ],
-        ],
-
-        plugins: [
-            '@babel/plugin-proposal-object-rest-spread',
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-transform-runtime',
-            '@babel/plugin-transform-object-assign',
-            '@babel/plugin-syntax-dynamic-import',
-            [
-                'import',
-                {
-                    libraryName: 'lodash',
-                    camel2DashComponentName: false,
-                    libraryDirectory: '',
-                    style: false,
-                },
-                'lodash',
-            ],
-            [
-                'import',
-                {
-                    libraryName: 'antd',
-                    style: 'css',
-                },
-                'antd',
-            ],
-            'react-hot-loader/babel',
-        ],
-    }
-}
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@src': './src',
+          },
+        },
+      ],
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-transform-object-assign',
+      '@babel/plugin-syntax-dynamic-import',
+      [
+        'import',
+        {
+          libraryName: 'lodash',
+          camel2DashComponentName: false,
+          libraryDirectory: '',
+          style: false,
+        },
+        'lodash',
+      ],
+      [
+        'import',
+        {
+          libraryName: 'antd',
+          style: 'css',
+        },
+        'antd',
+      ],
+      'react-hot-loader/babel',
+    ].concat(isProd ? ['transform-remove-console', 'transform-remove-debugger'] : []),
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 // Make sure react-hot-loader is required before react and react-dom
-import 'react-hot-loader'
+import 'react-hot-loader';
 
-import React from 'react'
-import ReactDom from 'react-dom'
-import moment from 'moment'
-import 'moment-timezone'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'connected-react-router'
-import { ConfigProvider } from 'antd'
+import React from 'react';
+import ReactDom from 'react-dom';
+import moment from 'moment';
+import 'moment-timezone';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { ConfigProvider } from 'antd';
 
-import zhCN from 'antd/lib/locale-provider/zh_CN'
-import App from '@/commons/app'
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import App from '@/commons/app';
 
-moment.locale('zh-cn') // default moment locale
+moment.locale('zh-cn'); // default moment locale
 
 // 初始化App对象
-App.init()
+App.init();
 
 // Render
-const Main = require('@/pages/main').default
+const Main = require('@/pages/main').default;
 
 const AppRoot = () => (
-    <Provider store={App.store}>
-        <ConfigProvider locale={zhCN}>
-            <ConnectedRouter history={App.history}>
-                <Main />
-            </ConnectedRouter>
-        </ConfigProvider>
-    </Provider>
-)
+  <Provider store={App.store}>
+    <ConfigProvider locale={zhCN}>
+      <ConnectedRouter history={App.history}>
+        <Main />
+      </ConnectedRouter>
+    </ConfigProvider>
+  </Provider>
+);
 
 // Render
 
-const rootEl = document.getElementById('app')
-ReactDom.render(<AppRoot />, rootEl)
+const rootEl = document.getElementById('app');
+ReactDom.render(<AppRoot />, rootEl);
